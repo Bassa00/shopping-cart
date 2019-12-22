952a10ac0b55e77cb64eb770b28afd7adae7e29b
brianttocs <brianttocs@gmail.com>
2019-12-22 23:04:30 +0200

    error page update

--- src/components/pages/Default.js
+++ src/components/pages/Default.js
@@ -9,7 +9,7 @@ export default class Default extends Component {
           <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
             <h1 className="display-3">404</h1>
             <h1>Error</h1>
-            <h2>Page not found!</h2>
+            <h2>Page Not Found!</h2>
             <h3>The requested URL 
               <span className="text-danger">      {this.props.location.pathname}
               </span> was not found!
